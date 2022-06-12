e04ab83b2b42939d4a036a3319ccdb93dbc247d7
Eduardo Carrion <eduardo.carrion@epn.edu.ec>
2022-06-11 21:10:04 -0500

    Homework Display Circles

--- ejercicio2D/circle.cpp
+++ ejercicio2D/circle.cpp
@@ -1,7 +1,15 @@
-//use -lGL -lGLU -lglut to link libraries
+/******************************************************
+Name: Eduardo Carrion
+
+This code is the homework for creatings circles
+to compile this code you need to link libraries
+use g++ circles.cpp -lGL -lGLU -lglut -o circles
+ ******************************************************/
 #include <iostream>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
+#include <math.h>
+#define PI 3,14
 
 void reshape(GLint w, GLint h){
     glViewport(0,0,w,h);
@@ -10,46 +18,53 @@ void reshape(GLint w, GLint h){
     gluOrtho2D(-100,100,-100,100);
 }
 
-void puntosDisp(){
-    glClear(GL_COLOR_BUFFER_BIT);
-    glPointSize(5);
-    glBegin(GL_POINTS);
-        glColor3f(1,1,0);
-        glVertex2f(2.5, 5.7);
+void circleByPointsDisp(){
+    int radius = 70;
 
-        glColor3f(1,0,0);
-        glVertex3f(50, 40, 0);
+    glClearColor(0.2,0.2,0.2,0.2);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPointSize(1);
+    glColor3f(0.5,0.5,1);
+    glBegin(GL_POINTS);
+        for (int i = 0; i < 124000; ++i) {
+            glVertex2f(cos(i) * radius, sin(i) * radius);
+        }
     glEnd();
+
+    radius = 52;
+    glColor3f(0.3,0.7,1);
+    glBegin(GL_POINTS);
+        for (int i = 0; i < 124000; ++i) {
+            glVertex2f(cos(i) * radius, sin(i) * radius);
+        }
+    glEnd();
+ 
     glutSwapBuffers();
 }
 
-void lineasDisp(){
+void circleByLinesDisp(){
+    int radius = 70;
+
     glClearColor(0.5,0.5,0.5,0.5);
     glClear(GL_COLOR_BUFFER_BIT);
     glLineWidth(1);
+    glColor3f(1,1,0);
     glBegin(GL_LINES);
-        glColor3f(1,1,0);
-        glVertex2i(50, -50);
-        glVertex2i(-45,45);
-    glEnd();
-
-    glLineWidth(10);
-    glBegin(GL_LINES);
-        glColor3f(1,0,0);
-        glVertex2i(-50,-50);
-        glVertex2i(45,45);
+        for (int i = 0; i < 4; ++i) {
+            glVertex2i(cos(i) * radius, sin(i) * radius);
+        }
     glEnd();
     glutSwapBuffers();
 }
 
 int main(int argc, char **argv){
-   glutInit(&argc, argv); 
-   glutInitDisplayMode(GLUT_RGB);
-   glutInitWindowSize(640, 480);
-   glutInitWindowPosition(100, 100);
-   glutCreateWindow("Ejemplo 2D");
-   glutDisplayFunc(lineasDisp);
-   glutReshapeFunc(reshape);
-  glutMainLoop(); 
+    glutInit(&argc, argv); 
+    glutInitDisplayMode(GLUT_RGB);
+    glutInitWindowSize(640, 640);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Homework Draw a Circle");
+    glutDisplayFunc(circleByPointsDisp);
+    glutReshapeFunc(reshape);
+    glutMainLoop(); 
     return 0;
 }
